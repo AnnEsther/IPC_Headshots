79103807b08ff313a4537983894dc2679273b18a
AnnJoy <annjoymathew@gmail.com>
2023-10-05 20:26:47 -0700

    Generated IPC 2013 LINK : https://github.com/AnnEsther/IPC_Headshots/blob/main/Output/2013.png

--- slack_workflow.py
+++ slack_workflow.py
@@ -5,8 +5,8 @@ URL = 'https://hooks.slack.com/triggers/T1R5KT477/5990957224085/ccb78233241dd72e
 
 
 def sendMessageToSlack(ipc):
-    p = str(round(100*(ipc/12000),2))
-    pBar = progressBar.getProgressBar(ipc) +" : "+p+" % "
+    p = str(round(100*(ipc/12000),1))
+    pBar = progressBar.getProgressBar(ipc) +" : "+p+"% "
     myobj = {
         "message": "Generated " +str(ipc)+ "\nhttps://github.com/AnnEsther/IPC_Headshots/blob/main/Output/"+str(ipc)+".png\n"+ pBar ,
         "ipc": ipc
